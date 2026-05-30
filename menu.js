2a795b798c05e4a9ffecf0204d83a8dd9cfcf88c
derrick mwenda <derrickmwenda75@gmail.com>
2026-05-30 09:59:11 +0300

    to production

--- menu.js
+++ menu.js
@@ -1,1 +1,14 @@
+function loadPDF(pdfFile) {
+    document.getElementById('pdf-viewer').innerHTML = `
+        <iframe
+            src="${pdfFile}"
+            width="100%"
+            height="800"
+            style="border:none;">
+        </iframe>
+    `;
 
+    document.getElementById('pdf-viewer').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
